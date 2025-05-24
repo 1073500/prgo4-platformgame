c8d868e1fd29107de254301932da478e90243772
1073500 <1073500@hr.nl>
2025-05-24 19:55:08 +0200

    bomb beweegt

--- src/js/bomb.js
+++ src/js/bomb.js
@@ -8,7 +8,15 @@ export class Bomb extends Actor {
         this.graphics.use(Resources.Bomb.toSprite())
         this.pos = new Vector(1280/2, 400)
         this.scale = new Vector(0.30, 0.30)
-        //this.anchor = new Vector(0., 0) //makkelijk afbeelding centeren
-    }
 
+
+        this.vel = new Vector(120, 0)
+        this.on('exitviewport', () => this.resetPosition())
+
+        
+
+    }
+    resetPosition() {
+        this.pos.x = 1280/2
+    }
 }
--- src/js/coins.js
+++ src/js/coins.js
@@ -6,9 +6,8 @@ export class Coin extends Actor {
         super()
 
         this.graphics.use(Resources.Coin.toSprite())
-        //this.pos = new Vector(400, 450 / 2)
+        this.pos = new Vector(580, 470)
         this.scale = new Vector(0.08, 0.08)
-        this.anchor = new Vector(0, 0) //makkelijk afbeelding centeren
 
     }
 
